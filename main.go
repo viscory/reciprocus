32db05297ea07403f6cca030d9d7cbcb4b797800
faiyaz <faiyaz_r@outlook.com>
2021-11-28 16:55:29 +0800

    initial

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "github.com/viscory/reciprocus/blockchain"
+)
+
+func main() {
+    chain := InitBlockChain()
+    chain.AddBlock("First")
+    chain.AddBlock("Second")
+    chain.AddBlock("Third")
+
+    for _,block := range chain.blocks {
+        fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+        fmt.Printf("Data in Block: %s\n", block.Data)
+        fmt.Printf("Hash: %x\n", block.Hash)
+        fmt.Printf("\n")
+    }
+}
